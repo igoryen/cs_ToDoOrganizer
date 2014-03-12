d6231b6549f9b4e5df5e2189b395b7098a1e6add
igoryen <igor.yentaltsev@gmail.com>
2014-03-12 10:50:20 -0400

    Global.asax/Database initializer: added

--- ToDoOrganizer/ToDoOrganizer/Global.asax.cs
+++ ToDoOrganizer/ToDoOrganizer/Global.asax.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿//=============================================================
+// 30. has definition of class Database
+// 35. has definition of MyDbContext
+//=============================================================
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity; //30
+using ToDoOrganizer.Models; // 35
 
 namespace ToDoOrganizer
 {
@@ -16,6 +22,7 @@ namespace ToDoOrganizer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<MyDbContext>(new MyDbInitializer()); // 1
         }
     }
 }
