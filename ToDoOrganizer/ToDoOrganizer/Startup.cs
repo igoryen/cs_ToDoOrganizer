423f80c1f3a1f8fac755dd10ae6059a3633f046b
igoryen <igor.yentaltsev@gmail.com>
2014-03-12 10:38:21 -0400

    After creating the project

---
+++ ToDoOrganizer/ToDoOrganizer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoOrganizer.Startup))]
+namespace ToDoOrganizer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
